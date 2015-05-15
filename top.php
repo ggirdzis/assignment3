fdde1baf81b4cd171a04e7d65b48c5f3283d91a4
Gretchen <ggirdzis@uvm.edu>
2015-05-15 11:21:46 -0400

    Assignment 3

---
+++ top.php
@@ -0,0 +1,27 @@
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <meta charset="UTF-8">
+        <meta content="Gretchen Girdzis" name="author">
+        <meta content="CS 142 Assignments" name="description">
+        
+        <link href="after.css" rel="stylesheet" type="text/css" media="screen" title="Style Sheet"/>
+       
+        <HEAD>
+        <TITLE>Vermont Roller Skating Rinks - Web Sites - Roller Rinks in Vermont</title>
+        <META NAME="Keywords" CONTENT="vermont roller skating rinks
+              vermont roller rinks">
+        <META NAME="Description" CONTENT="Where to find roller
+              rinks in the U.S. state of Vermont.  Includes official
+              Web sites of Vermont roller skating rinks.">
+    </HEAD>
+    </head>
+    
+
